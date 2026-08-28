35616b50c7a933b0dfa45b2fcb08fea6f18b3ad0
flesentine <chrisgmcc@gmail.com>
2026-08-28 07:42:29 -0700

    Run FALL accessibility sidebar patch last

--- scripts/fix_debug_canvas_restore.py
+++ scripts/fix_debug_canvas_restore.py
@@ -10,3 +10,4 @@ here = Path(__file__).resolve().parent
 runpy.run_path(str(here / 'fix_debug_canvas_restore_base.py'), run_name='__main__')
 runpy.run_path(str(here / 'patch_web_debug_view_presets.py'), run_name='__main__')
 runpy.run_path(str(here / 'patch_web_debug_view_bridge.py'), run_name='__main__')
+runpy.run_path(str(here / 'patch_web_fall_accessibility.py'), run_name='__main__')
