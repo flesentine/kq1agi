fafe18f7aa0f75c2c121901f4320d9a414e44607
flesentine <chrisgmcc@gmail.com>
2026-08-29 09:00:08 -0700

    Run simplified FALL legend after scan diagnostics

--- scripts/fix_debug_canvas_restore.py
+++ scripts/fix_debug_canvas_restore.py
@@ -12,3 +12,4 @@ runpy.run_path(str(here / 'patch_web_debug_view_presets.py'), run_name='__main__
 runpy.run_path(str(here / 'patch_web_debug_view_bridge.py'), run_name='__main__')
 runpy.run_path(str(here / 'patch_web_fall_accessibility.py'), run_name='__main__')
 runpy.run_path(str(here / 'patch_web_fall_scan_refresh.py'), run_name='__main__')
+runpy.run_path(str(here / 'simplify_fall_legend.py'), run_name='__main__')
