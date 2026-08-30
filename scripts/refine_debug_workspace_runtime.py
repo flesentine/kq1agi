a8b895de245b88c53e678f94b7564cd6139b58d7
flesentine <chrisgmcc@gmail.com>
2026-08-30 11:58:45 -0700

    Run direct DEBUG TEST bridge patch

--- scripts/refine_debug_workspace_runtime.py
+++ scripts/refine_debug_workspace_runtime.py
@@ -11,6 +11,7 @@ runpy.run_path(str(here / 'refine_debug_workspace_runtime_base.py'), run_name='_
 runpy.run_path(str(here / 'fix_debug_view_presets_runtime.py'), run_name='__main__')
 runpy.run_path(str(here / 'fix_debug_view_bridge_runtime.py'), run_name='__main__')
 runpy.run_path(str(here / 'fix_debug_eraser_bridge_runtime.py'), run_name='__main__')
+runpy.run_path(str(here / 'fix_debug_test_bridge_runtime.py'), run_name='__main__')
 runpy.run_path(str(here / 'improve_fall_accessibility_runtime.py'), run_name='__main__')
 runpy.run_path(str(here / 'fix_scene_seed_tick.py'), run_name='__main__')
 runpy.run_path(str(here / 'fix_shared_array_signed_reads.py'), run_name='__main__')
